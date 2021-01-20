cd6b8105582e32b05b71eb13311f50214cf7fa7c
iyaaaan <christianocol12@gmail.com>
2021-01-20 15:24:40 +0800

    minify banners/ fixed httpaccess/error404.php

--- error404.php
+++ error404.php
@@ -2,39 +2,56 @@
 <html lang="en">
 
 <head>
-	<?php include('include-header.php'); ?>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
+	<title>Page Not Found | Artine Photography</title>
 
-	<title>A r t i N e | ERROR 404</title>
+	<style>
+		.error {
+			background: url(../img/banners/error404.jpg);
+			background-attachment: fixed;
+			background-size: cover;
+			background-repeat: no-repeat;
+			background-position: center;
+			color: var(--white);
+			text-align: center;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			position: relative;
+			height: 100vh;
+			width: 100%;
+			max-height: 100vh;
+			padding: 1.5rem;
+		}
 
-	<!-- Font Awesome 4.7.0 -->
-	<link rel="stylesheet" href="css/font-awesome.min.css">
+		.error__header {
+			color: var(--white);
+			font-size: 40px;
+			font-family: var(--font-header);
+			line-height: 1;
+			margin-top: -50px;
+			margin-bottom: 30px;
+		}
 
-	<!-- AOS -->
-	<link rel="stylesheet" href="css/aos.min.css">
+		@media screen and (min-width: 768px) {
+			.error__header {
+				font-size: var(--font-large);
+			}
+		}
 
-	<!-- compiled css styles -->
-	<link rel="preload" href="css/main.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+	</style>
 
 </head>
 
 <body>
 
-	<!-- include pre-loader	-->
-	<?php include('include-preloader.php');  ?>
-
-	<div class="error text-white text-center p-1">
+	<div class="error">
 		<h1 class="error__header">404! Looks like we didn't capture that one!</h1>
 		<p>Kindly check your link and make sure it's the right one. ;)</p>
 
 		<a href="index.php" class="btn-outline btn-outline--light btn--large mt-5">Go Home</a>
 	</div>
-
-	<!-- External JS Files -->
-	<script src="js/jquery.min.js"></script>
-
-	<!--Main Script-->
-	<script src="js/main.min.js"></script>
-
 
 </body>
 
